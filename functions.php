3bc49f7c2e425b753bd2ed8bb411d546c487e528
Ben Kaminski <ben@benkaminski.com>
2018-04-05 17:17:26 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function enqueue_crafted_brew_scripts()
     wp_enqueue_script('Ajax-Popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', false, null, true, null);
     wp_enqueue_script('Bootstrap-4x', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), null, true, null);
     wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/releases/v5.0.6/js/all.js', false, null, true, null);
-    wp_enqueue_script('blueEarl-scripts', get_template_directory_uri() . '/js/craftedBrew.js', array('jquery'), null, true, null);
+    wp_enqueue_script('crafted-brew-scripts', get_template_directory_uri() . '/js/craftedBrew.js', array('jquery'), null, true, null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_crafted_brew_scripts');
 
@@ -14,7 +14,7 @@ add_action('wp_enqueue_scripts', 'enqueue_crafted_brew_scripts');
 function enqueue_crafted_brew_styles()
 {
     wp_enqueue_style('bootstrap4.0-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), null);
-    wp_enqueue_style('blueEarl-styles', get_template_directory_uri() . '/style.css', array(), null);
+    wp_enqueue_style('crafted-brew-styles', get_template_directory_uri() . '/style.css', array(), null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_crafted_brew_styles');
 
