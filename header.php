31fa82b0eac9a614ad17ccd36945e7c44fbdef7b
Ben Kaminski <ben@benkaminski.com>
2018-04-11 18:35:40 -0400

    header html tag should sit outside of nav html tag, not wrap it. fixed

--- header.php
+++ header.php
@@ -23,29 +23,30 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">Company Name</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<?php
+				wp_nav_menu(array(
+					'theme_location' => 'primary',
+					'depth' => 2,
+					'container' => '',
+					'container_class' => '',
+					'container_id' => '',
+					'menu_class' => 'navbar-nav mr-auto',
+					'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+					'walker' => new WP_Bootstrap_Navwalker()
+				));
+				?>
+				<form class="form-inline mt-2 mt-md-0">
+					<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
 		<header>
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">Company Name</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarCollapse">
-					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'primary',
-						'depth' => 2,
-						'container' => '',
-						'container_class' => '',
-						'container_id' => '',
-						'menu_class' => 'navbar-nav mr-auto',
-						'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-						'walker' => new WP_Bootstrap_Navwalker()
-						));
-					?>
-					<form class="form-inline mt-2 mt-md-0">
-						<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</form>
-				</div>
-			</nav>
+			
 		</header>
