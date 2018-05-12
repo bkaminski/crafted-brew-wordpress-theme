0e1a8ea81d9fca90231e28bab1ef74e1254abf7f
Ben Kaminski <ben@benkaminski.com>
2018-05-12 11:58:10 -0400

    added poster image to replace video at mobile

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<header>
-		<div class="col pl-0 pr-0">
-			<video autoplay="true" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
+		<div class="col pl-0 pr-0 header-content">
+			<video autoplay="true" id="video-background" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
 			<!-- loading fallback -->
 				<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-white"></i>
 				<p class="text-center">Your browser may not support HTML5 video</p>
