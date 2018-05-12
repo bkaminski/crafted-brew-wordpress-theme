0dea0376d80de807a7442ef1fb47192a50a36cbb
Ben Kaminski <ben@benkaminski.com>
2018-05-11 20:18:43 -0400

    code cleanup

--- header.php
+++ header.php
@@ -44,14 +44,14 @@
 					));
 				?>
 			</div>
-			<div class="social-header my-2 my-lg-0">
-				<a href="https://www.facebook.com/blueearlbrewing/" class="" target="_blank">
+			<div class="my-2 my-lg-0">
+				<a href="https://www.facebook.com/blueearlbrewing/" target="_blank">
 					<i class="fab fa-facebook-square fa-fw fa-lg text-white mr-2"></i>
 				</a>
-				<a href="https://twitter.com/blueearlbeer" class="" target="_blank">
+				<a href="https://twitter.com/blueearlbeer" target="_blank">
 					<i class="fab fa-twitter-square fa-fw fa-lg text-white mr-2"></i>
 				</a>
-				<a href="https://www.instagram.com/blueearlbeer/?hl=en" class="" target="_blank">
+				<a href="https://www.instagram.com/blueearlbeer/?hl=en" target="_blank">
 					<i class="fab fa-instagram fa-fw fa-lg text-white mr-5"></i>
 				</a>
 				<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brew-for-your-soul.png" alt="Brew For Your Soul" />
