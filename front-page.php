4dbb9a1bfd4efdd6d16948b87bd6001150e98b88
Ben Kaminski <ben@benkaminski.com>
2018-05-13 19:56:23 -0400

    set up initial carousel

--- front-page.php
+++ front-page.php
@@ -1,23 +1,26 @@
 <?php get_header(); ?>
-		<div class="container-lg header-content">
-			<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-logo.png" class="blue-earl-logo img-fluid mx-auto d-block" alt="Blue Earl Brewing Company" />
-			<video autoplay="true" id="video-background" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
-				<!-- loading fallback -->
-				<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-white"></i>
-				<p class="text-center">Your browser may not support HTML5 video</p>
-				<span class="sr-only">Video of beer being poured into pint glass.</span>
-			</video>
-		</div>
-		<div class="container-lg book-tour">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-9">
-						<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
-					</div>
-					<div class="col-md-3">
-						<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1" href="#">Book Tour</a>
-					</div>
+	<div class="container-lg header-content">
+		<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-logo.png" class="blue-earl-logo img-fluid mx-auto d-block" alt="Blue Earl Brewing Company" />
+		<video autoplay="true" id="video-background" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
+			<!-- loading fallback -->
+			<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-white"></i>
+			<p class="text-center">Your browser may not support HTML5 video</p>
+			<span class="sr-only">Video of beer being poured into pint glass.</span>
+		</video>
+	</div>
+	<div class="container-lg book-tour">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
+				</div>
+				<div class="col-md-3">
+					<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1" href="#">Book Tour</a>
 				</div>
 			</div>
-		</div>
+		</div>	
+	</div>
+	<div class="container-lg">
+		<?php include get_template_directory() . '/lib/inc/blue-earl-carousel.php'; ?>
+	</div>
 <?php get_footer(); ?>
---
+++ lib/inc/blue-earl-carousel.php
@@ -0,0 +1,26 @@
+		<div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-04.jpg" alt="First slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-02.png" alt="Second slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-03.jpg" alt="Third slide">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
