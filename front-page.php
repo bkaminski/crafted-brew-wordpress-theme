5811c2644d525c4fdd60092835e1f97ac4437a14
Ben Kaminski <ben@benkaminski.com>
2018-05-13 20:10:55 -0400

    made button text-uppercase

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@
 					<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
 				</div>
 				<div class="col-md-3">
-					<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1" href="#">Book Tour</a>
+					<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1 text-uppercase" href="#">Book Tour</a>
 				</div>
 			</div>
 		</div>	
