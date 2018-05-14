7901baee8ab4a7ce661a73871b769b4283d8d43a
Ben Kaminski <ben@benkaminski.com>
2018-05-13 20:28:46 -0400

    Update blue-earl-carousel.php

--- lib/inc/blue-earl-carousel.php
+++ lib/inc/blue-earl-carousel.php
@@ -15,11 +15,11 @@
 					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-03.jpg" alt="Third slide">
 				</div>
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
 				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				<span class="sr-only">Previous</span>
 			</a>
-			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
 				<span class="sr-only">Next</span>
 			</a>
