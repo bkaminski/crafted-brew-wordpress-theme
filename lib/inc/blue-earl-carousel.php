453730f9e8973c64b8f1cb5998bc8d04ed1b2b0c
Ben Kaminski <ben@benkaminski.com>
2018-05-14 14:02:19 -0400

    added more images to carousel
    need to discuss image limit with client.

--- lib/inc/blue-earl-carousel.php
+++ lib/inc/blue-earl-carousel.php
@@ -3,16 +3,32 @@
 				<li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
 				<li data-target="#carouselIndicators" data-slide-to="1"></li>
 				<li data-target="#carouselIndicators" data-slide-to="2"></li>
+				<li data-target="#carouselIndicators" data-slide-to="3"></li>
+				<li data-target="#carouselIndicators" data-slide-to="4"></li>
+				<li data-target="#carouselIndicators" data-slide-to="5"></li>
+				<li data-target="#carouselIndicators" data-slide-to="6"></li>
 			</ol>
 			<div class="carousel-inner" style="max-height:632px;">
-				<div class="carousel-item active">
+				<div class="carousel-item car-img active">
 					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-04.jpg" alt="First slide">
 				</div>
-				<div class="carousel-item">
+				<div class="carousel-item car-img">
 					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-02.png" alt="Second slide">
 				</div>
-				<div class="carousel-item">
+				<div class="carousel-item car-img">
 					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-03.jpg" alt="Third slide">
+				</div>
+				<div class="carousel-item car-img">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-05.jpg" alt="Fourth slide">
+				</div>
+				<div class="carousel-item car-img">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-06.jpg" alt="Fifth slide">
+				</div>
+				<div class="carousel-item car-img">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-07.png" alt="Sixth slide">
+				</div>
+				<div class="carousel-item car-img">
+					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-09.png" alt="Seventh slide">
 				</div>
 			</div>
 			<a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
