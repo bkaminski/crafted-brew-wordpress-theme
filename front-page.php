00a013e7ab3b10fff4fe13b70e25b16e3bef3ed5
Ben Kaminski <ben@benkaminski.com>
2018-05-14 10:34:23 -0400

    added blues society partnership section

--- front-page.php
+++ front-page.php
@@ -1,53 +1,69 @@
 <?php get_header(); ?>
-	<div class="container-lg header-content">
-		<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-logo.png" class="blue-earl-logo img-fluid mx-auto d-block" alt="Blue Earl Brewing Company" />
-		<video autoplay="true" id="video-background" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
-			<!-- loading fallback -->
-			<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-white"></i>
-			<p class="text-center">Your browser may not support HTML5 video</p>
-			<span class="sr-only">Video of beer being poured into pint glass.</span>
-		</video>
-	</div>
-	<div class="container-lg book-tour">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9">
-					<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
+	<header>
+		<div class="container-lg header-content">
+			<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-logo.png" class="blue-earl-logo img-fluid mx-auto d-block" alt="Blue Earl Brewing Company" />
+			<video autoplay="true" id="video-background" class="beer-overflow" muted="" plays-inline loop="true" src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-brewing.mp4" type="video/mp4">
+				<!-- loading fallback -->
+				<i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-white"></i>
+				<p class="text-center">Your browser may not support HTML5 video</p>
+				<span class="sr-only">Video of beer being poured into pint glass.</span>
+			</video>
+		</div>
+	</header>
+	<section>
+		<div class="container-lg book-tour">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9">
+						<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
+					</div>
+					<div class="col-md-3">
+						<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1 text-uppercase" href="#">Book Tour</a>
+					</div>
 				</div>
-				<div class="col-md-3">
-					<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1 text-uppercase" href="#">Book Tour</a>
+			</div>	
+		</div>
+	<section>
+	<main>
+		<div class="container-lg">
+			<?php include get_template_directory() . '/lib/inc/blue-earl-carousel.php'; ?>
+		</div>
+	</main>
+	<section>
+		<div class="container-lg book-tour">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-9">
+						<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
+					</div>
+					<div class="col-md-3">
+						<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1 text-uppercase" href="#">Book Tour</a>
+					</div>
 				</div>
-			</div>
-		</div>	
-	</div>
-	<div class="container-lg">
-		<?php include get_template_directory() . '/lib/inc/blue-earl-carousel.php'; ?>
-	</div>
-	<div class="container-lg book-tour">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9">
-					<p class="gold-text pt-4 text-uppercase pf-1">Book your next brewery tour now by calling 302.653.BEER</p>
+			</div>	
+		</div>
+	<section>
+	<aside>
+		<div class="container-lg testimonail-outer">
+			<div class="container">
+				<div class="testimonials">
+					<p class="text-center text-white text-shadow pb-3">"My favorite craft beer at my favorite brewery! And the entertainment they have....second to none!!! Their blues jam every month is not to be missed!"
+					<br />
+					<small><cite>- Judy M. Mangini</cite></small></p>
+					<p class="text-center text-white text-shadow pb-3">"Great beer, great staff, great ambiance, great jamming music; a warm relaxing experience...highly recommended!"
+				    <br />
+					<cite><small>- Philip Ardanuy</small></cite>
+					<p class="text-center text-white text-shadow pb-3">"The beer is magical, the atmosphere cozy and the folks who own & operate it can't be any nicer!"
+				    <br />
+					<cite><small>- Tara K. Schriver</small></cite>
 				</div>
-				<div class="col-md-3">
-					<a class="ml-5 btn btn-lg btn-warning mt-3 mb-2 btn-1 text-uppercase" href="#">Book Tour</a>
+				<div class="col pt-3">
+					<p class="text-center text-white text-uppercase">In partnership with Blue Earl Brewing</p>
+					<p class="text-center pb-4">
+						<img src="<?php echo get_template_directory_uri(); ?>/lib/img/central-de-blues-society.jpg" alt="Central Delaware Blues Society" />
+					</p>
 				</div>
-			</div>
-		</div>	
-	</div>
-	<div class="container-lg testimonail-outer">
-		<div class="container">
-			<div class="testimonials">
-				<p class="text-center text-white display-4 text-shadow pb-3">"My favorite craft beer at my favorite brewery! And the entertainment they have....second to none!!! Their blues jam every month is not to be missed!"
-				<br />
-				<small><cite>- Judy M. Mangini</cite></small></p>
-				<p class="text-center text-white display-4 text-shadow pb-3">"Great beer, great staff, great ambiance, great jamming music; a warm relaxing experience...highly recommended!"
-			    <br />
-				<cite><small>- Philip Ardanuy</small></cite>
-				<p class="text-center text-white display-4 text-shadow pb-3">"The beer is magical, the atmosphere cozy and the folks who own & operate it can't be any nicer!"
-			    <br />
-				<cite><small>- Tara K. Schriver</small></cite>
 			</div>
 		</div>
-	</div>
+	</aside>
 <?php get_footer(); ?>
