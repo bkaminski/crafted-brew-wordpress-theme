6e01e919b2c5182f0f11e7f54e32187897f6df70
Ben Kaminski <ben@benkaminski.com>
2018-05-14 13:20:54 -0400

    added max-height to carousel

--- lib/inc/blue-earl-carousel.php
+++ lib/inc/blue-earl-carousel.php
@@ -4,7 +4,7 @@
 				<li data-target="#carouselIndicators" data-slide-to="1"></li>
 				<li data-target="#carouselIndicators" data-slide-to="2"></li>
 			</ol>
-			<div class="carousel-inner">
+			<div class="carousel-inner" style="max-height:632px;">
 				<div class="carousel-item active">
 					<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/lib/img/carousel-04.jpg" alt="First slide">
 				</div>
