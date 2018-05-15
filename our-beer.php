1496d274fce008c6c66f40f7d39c8d5f4e638ab2
Ben Kaminski <ben@benkaminski.com>
2018-05-15 14:33:35 -0400

    added page for our beer section and cooresponding CSS

---
+++ our-beer.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template Name: Our Beer Page Layout
+ *
+ * @package WordPress
+ * @subpackage crafted-brew-wordpress-theme
+ * 
+ */
+get_header(); ?>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
+<?php get_footer(); ?>
