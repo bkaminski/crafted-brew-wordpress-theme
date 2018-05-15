15b517e16bb494e1e6e5f3981026758aec566073
Ben Kaminski <ben@benkaminski.com>
2018-05-15 15:32:31 -0400

    added custom page for our beer section and styled.

--- our-beer.php
+++ our-beer.php
@@ -7,64 +7,25 @@
  * 
  */
 get_header(); ?>
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-<br />
-
-
+<div class="container our-beer">
+	<div class="row">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<div class="col">
+			<h1 class="text-white mb-5 text-uppercase"><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<div class="row mt-5">
+		<div class="col-md-4">
+			<?php the_post_thumbnail('full'); ?>
+		</div>
+		<div class="col-md-8 text-white">
+			<?php the_content(); ?>
+			<br />
+			<?php the_tags('Related Topics: ' , ', ', ''); ?>
+		</div>
+	</div>
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+</div>
 <?php get_footer(); ?>
