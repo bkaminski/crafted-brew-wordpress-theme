38aa1864b4867de8320d57582a0554d5bae5d7f5
Ben Kaminski <ben@benkaminski.com>
2018-05-17 17:42:12 -0400

    made font color white on excerpt

--- tag.php
+++ tag.php
@@ -16,7 +16,10 @@
 			<main>
 				<h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 					<?php the_title(); ?></a></h1>
-				<?php the_excerpt(); ?>
+					<div class="text-white">
+						<?php the_excerpt(); ?>
+						
+					</div>
 				<hr />
 			</main>
 			<br />
