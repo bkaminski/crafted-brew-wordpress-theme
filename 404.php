1dceb3ba55d75bdde286910c9fe76e263de75e87
Ben Kaminski <ben@benkaminski.com>
2018-05-19 14:01:51 -0400

    created 404.php and styled EME calendar with CSS
    removed jQuery styling, ajax pagination was causing errors.

--- 404.php
+++ 404.php
@@ -1,6 +1,22 @@
 <?php get_header(); ?>
-
-
-
-
+<div class="container brewery">
+	<div class="row">
+		<header>
+			<div class="col">
+				<h1 class="text-white mb-5 text-uppercase"><?php _e( '404: Page Not Found', 'crafted_brew' ); ?></h1>
+			</div>
+		</header>
+	</div>
+	<div class="row mt-5">
+		<div class="col-md-12 mb-4">
+			<main>
+				<h2><?php _e( 'Have a few too many Blue Earl beverages?', 'crafted_brew' ); ?></h2>
+				<p><?php _e( 'The page you are looking for has either been moved or never existed in the first place. Please choose an option from the above menu, or try clicking the back browser button.', 'crafted_brew' ); ?>
+					
+				</p>
+			</main>
+			<br />
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
