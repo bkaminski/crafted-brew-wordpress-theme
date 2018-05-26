3581c8ba846d98abf26451b0a6625daf9267dca9
Ben Kaminski <ben@benkaminski.com>
2018-05-26 17:52:52 -0400

    no GA tracking

--- header.php
+++ header.php
@@ -1,25 +1,6 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119885337-1"></script>
-		<script>
-	  		window.dataLayer = window.dataLayer || [];
-	  		function gtag(){dataLayer.push(arguments);}
-	  		gtag('js', new Date());
-
-  			gtag('config', 'UA-119885337-1');
-		</script>
-		<!-- Google Tag Manager -->
-		<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','GTM-MQJV9SV');
-		</script>
-		<!-- End Google Tag Manager -->
-
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="msvalidate.01" content="E4F5FA02C51943257359F1B364428943" />
@@ -44,9 +25,6 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQJV9SV" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->
 		<nav class="navbar navbar-expand-md fixed-top navbar-dark">
 			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 				<img src="<?php echo get_template_directory_uri(); ?>/lib/img/blue-earl-icon-web.png" alt="Blue Earl Brewing" />
