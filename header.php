90b578f83cf2ea3e65c29929f2448539a5aed94a
Ben Kaminski <ben@benkaminski.com>
2018-06-26 17:34:05 -0400

    removed ga from header.php

--- header.php
+++ header.php
@@ -1,15 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119885337-1"></script>
-		<script>
-	  		window.dataLayer = window.dataLayer || [];
-	  		function gtag(){dataLayer.push(arguments);}
-	  		gtag('js', new Date());
-
-  			gtag('config', 'UA-119885337-1');
-		</script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="msvalidate.01" content="E4F5FA02C51943257359F1B364428943" />
